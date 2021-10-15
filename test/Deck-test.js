1d76f3a5b56b8f3da9a7b71956e2b12112ed77fd
Regan Losey <regan.losey@gmail.com>
2021-10-14 17:45:43 -0700

    Refactor test files to use beforeEach and fix calculatePercent function

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -5,16 +5,24 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
 describe('Deck', function () {
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+
+  beforeEach(function () {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    deck = new Deck([card1, card2, card3]);
+  })
+  
   it('should be a function', function () {
-    const deck = new Deck()
+
     expect(Deck).to.be.a('function');
   })
 
   it('should be able to count the cards in the deck', function () {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-    const deck = new Deck([card1, card2, card3]);
     deck.countCards();
     expect(deck.cards.length).to.equal(3);
   })
